7823effdce5e44f986e432d4f960f183fb84d087
vivian waithera <vivyannekori@gmail.com>
2023-08-25 13:53:14 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+if __name__ == "__main__":
+    # Test the Shoe class
+    stan_smith = Shoe("Adidas", 9)
+    print(stan_smith.size)  
+    stan_smith.size = "not an integer"  
+    print(stan_smith.size)  
+    stan_smith.cobble()  
